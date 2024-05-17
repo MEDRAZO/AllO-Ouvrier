4f2e170b5a1eff6460f00286757ef8f981d964b2
msebbar <msebbar@sqli.com>
2024-05-17 22:03:19 +0000

    Creation of a login form

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,12 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[isGranted("ROLE_USER")]
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        return $this->render('home/index.html.twig',[
+            'user' => $this->getUser(),
+        ]);
     }
 }
